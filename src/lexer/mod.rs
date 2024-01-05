77a05489e9d3ce8eb97f5f0fbc23b0976556f6cb
Tomáš Ďuriš <tomas.duris@gendigital.com>
2024-01-05 10:27:50 +0100

    Implement token conversion for lexer

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -1,11 +1,24 @@
+use crate::{parser::syntaxkind::SyntaxKind, syntax::syntax_error::SyntaxError};
 use logos::Logos;
+use std::fmt;
 use std::num::ParseIntError;
+use text_size::{TextRange, TextSize};
 
 #[derive(Default, Debug, Clone, PartialEq)]
 pub(crate) enum LexingError {
     InvalidInteger(String),
     #[default]
     InvalidCharacter,
+}
+
+// Implement Display trait for LexingError
+impl fmt::Display for LexingError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            LexingError::InvalidInteger(msg) => write!(f, "Invalid integer: {}", msg),
+            LexingError::InvalidCharacter => write!(f, "Invalid character"),
+        }
+    }
 }
 
 /// Error type returned by calling `lex.slice().parse()` to u8.
@@ -21,7 +34,7 @@ impl From<ParseIntError> for LexingError {
 
 #[derive(Logos, Debug, PartialEq)]
 #[logos(error = LexingError)]
-pub(crate) enum Token {
+pub(crate) enum LogosToken {
     // For now it is desired to support only small subset of YARA language
     // so just something like:
     // rule foo {
@@ -89,4 +102,83 @@ pub(crate) enum Token {
     Comment,
     #[regex(r"/\*([^*]|\*[^/])*\*/")]
     MultilineComment,
+}
+
+/// A token of Rust source.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Token {
+    /// The kind of token.
+    pub kind: SyntaxKind,
+    /// The length of the token.
+    pub len: TextSize,
+}
+
+pub fn tokenize(text: &str) -> (Vec<Token>, Vec<SyntaxError>) {
+    if text.is_empty() {
+        return Default::default();
+    }
+
+    let mut tokens = Vec::new();
+    let mut errors = Vec::new();
+    let mut offset = 0;
+
+    let logos_tokens: Vec<_> = LogosToken::lexer(&text).spanned().collect();
+
+    // Loop over all tokens, convert them to syntaxkind and push them into tokens vector
+    // also push errors into errors vector
+    for (token, range) in logos_tokens {
+        let token_len = range.len().try_into().unwrap();
+        let token_range = TextRange::at(offset.try_into().unwrap(), token_len);
+        let syntaxkind;
+        match token {
+            Ok(token) => {
+                syntaxkind = logos_tokenkind_to_syntaxkind(token);
+            }
+            Err(err) => {
+                errors.push(SyntaxError::new(err.to_string(), token_range));
+                syntaxkind = SyntaxKind::ERROR;
+            }
+        }
+        tokens.push(Token {
+            kind: syntaxkind,
+            len: token_len,
+        });
+        offset += range.len();
+    }
+
+    // Add EOF token at the end
+    tokens.push(Token {
+        kind: SyntaxKind::EOF,
+        len: 0.into(),
+    });
+
+    (tokens, errors)
+}
+
+// Convert LogosToken to SyntaxKind
+fn logos_tokenkind_to_syntaxkind(token: LogosToken) -> SyntaxKind {
+    match token {
+        LogosToken::Rule => SyntaxKind::RULE,
+        LogosToken::Strings => SyntaxKind::STRINGS,
+        LogosToken::Condition => SyntaxKind::CONDITION,
+        LogosToken::And => SyntaxKind::AND,
+        LogosToken::Or => SyntaxKind::OR,
+        LogosToken::Not => SyntaxKind::NOT,
+        LogosToken::Identifier(_) => SyntaxKind::IDENTIFIER,
+        LogosToken::Variable(_) => SyntaxKind::VARIABLE,
+        LogosToken::String(_) => SyntaxKind::STRING,
+        LogosToken::Assign => SyntaxKind::ASSIGN,
+        LogosToken::Colon => SyntaxKind::COLON,
+        LogosToken::LBrace => SyntaxKind::LBRACE,
+        LogosToken::RBrace => SyntaxKind::RBRACE,
+        LogosToken::LParen => SyntaxKind::LPAREN,
+        LogosToken::RParen => SyntaxKind::RPAREN,
+        LogosToken::Comma => SyntaxKind::COMMA,
+        LogosToken::Number(_) => SyntaxKind::NUMBER,
+        LogosToken::True => SyntaxKind::TRUE,
+        LogosToken::False => SyntaxKind::FALSE,
+        LogosToken::Whitespace => SyntaxKind::WHITESPACE,
+        LogosToken::Comment => SyntaxKind::COMMENT,
+        LogosToken::MultilineComment => SyntaxKind::MULTILINECOMMENT,
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
+use lexer::LogosToken;
+use logos::Logos;
 /// This library is used to create a parser for YARA language
 /// It should provide also token for whitespaces
 /// as we want full fidelity and error resilience.;
 use std::{env::args, fs, path::Path};
-use lexer::Token;
-use logos::Logos; 
+
+use crate::lexer::tokenize;
 
 mod lexer;
+mod parser;
+mod syntax;
 
 fn main() {
     // Take file as an input and parse it into tokens
@@ -13,11 +17,6 @@ fn main() {
     let path = Path::new(&arg);
     let input = fs::read_to_string(path).unwrap();
 
-    // print all tokens
-    for result in Token::lexer(&input) {
-        match result {
-            Ok(token) => println!("{:?}", token),
-            Err(err) => println!("{:?}", err),
-        }
-    }
+    let tokens = tokenize(&input);
+    println!("{:?}", tokens);
 }
