2668bd2801aac0860ecf0e0b4c458cc1f69d9db5
Tomáš Ďuriš <tomas.duris@gendigital.com>
2024-03-25 10:38:23 +0100

    fix clippy warnings

--- src/bin.rs
+++ src/bin.rs
@@ -4,7 +4,7 @@ use yara_parser::{AstNode, SourceFile};
 fn main() {
     let filename = std::env::args().nth(1).expect("No arguments provided");
     let file_content =
-        fs::read_to_string(&filename).expect("Something went wrong while reading the file");
+        fs::read_to_string(filename).expect("Something went wrong while reading the file");
     let ast = SourceFile::parse(&file_content);
     println!("AST:\n\n{:#?}", ast.tree().syntax());
     println!("Errors: {:?}", ast.errors());
